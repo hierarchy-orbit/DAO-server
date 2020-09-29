a5d05637cde12660fdd7610e9b15bb4a3a2682fb
JafferAbbas-codes <beastincarnate121@gmail.com>
2020-09-30 03:34:11 +0500

    Changed port

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(4000);
+  await app.listen(process.env.port|| 4000);
   // await votingResultCalculation();
 }
 bootstrap();
