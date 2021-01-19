b35e7446ab12f447fe8ab9b6e1b24efe10c277fd
Samad191 <samad13354@gmail.com>
2021-01-19 16:19:11 +0500

    Email image issue resolving

--- src/nodemailer/nodemailer.service.ts
+++ src/nodemailer/nodemailer.service.ts
@@ -81,8 +81,8 @@ export class NodemailerService {
         `,
         attachments: [
           {
-            filename: 'Logo.png',
-            path: './logo.png',
+            filename: 'logo.png',
+            path: './assets/logo.png',
             cid: 'logo', //my mistake was putting "cid:logo@cid" here!
           },
         ],
@@ -102,122 +102,7 @@ export class NodemailerService {
       throw e;
     }
   };
-  //function to send invoice mail to payer
-  // sendInvoiceToPayerEmail = async (invoiceData) => {
-  //   try {
-  //     console.log("send Invoice Mail");
-  //     const code = Math.floor(Math.random() * 1000000);
-  //     let info = await transporter.sendMail({
-  //       from: "", // sender address
-  //       to: "" + `${invoiceData.email}`, //receiver email
-  //       subject: "Telemed.App Verification Code", // Subject line
-  //       html: `
-  //               <br/>
-  //               <p>Dear ${invoiceData.name},</p>
-  //               <p><span>Your Verification Code is: <b> ${code} <b/><span/></p>`,
-  //     });
-  //     const mail = {
-  //       info: info,
-  //     };
-  //     return code;
-  //   } catch (e) {
-  //     console.log("catching error in mail==>", e);
-  //     throw "Email not Send";
-  //   }
-  // };
-  // sendMailToContactUs = async (invoiceData) => {
-  //   try {
-  //     let info = await transporter.sendMail({
-  //       from: "", // sender address
-  //       to: `telemedapp.international@gmail.com`, //receiver email
-  //       subject: "Telemed.App Contact Us", // Subject line
-  //       html: `
-  //               <br/>
-  //               <p>Name: ${invoiceData.name},</p>
-  //               <p><span>Message: ${invoiceData.message}<b/><span/></p>`,
-  //     });
-  //     const mail = {
-  //       info: info,
-  //     };
-  //     return mail;
-  //   } catch (e) {
-  //     console.log("catching error in mail==>", e.message);
-  //     throw "Email not Send";
-  //   }
-  // };
-  // Test = async () => {
-  //   try {
-  //     let info = await transporter.sendMail({
-  //       from: "", // sender address
-  //       to: "" + `${"sarfarazahmedkhankhan@gmail.com"}`, //receiver email
-  //       subject: "Telemed.App report cron job Us", // Subject line
-  //       html: `
-  //               <br/>
-  //               <p><span>Message:<b/><span/></p>`,
-  //     });
-  //     const mail = {
-  //       info: info,
-  //     };
-  //     return mail;
-  //   } catch (e) {
-  //     console.log("catching error in mail==>", e);
-  //     throw e;
-  //   }
-  // };
-  // appointmentConfirmation = async (
-  //   invoiceData,
-  //   appointment_details,
-  //   doctor
-  // ) => {
-  //   try {
-  //     let info = await transporter.sendMail({
-  //       from: "", // sender address
-  //       to: "" + `${invoiceData.email}`, //receiver email
-  //       subject: "Telemed.App Appointment Confirmation", // Subject line
-  //       html: `
-  //               <br/>
-  //               <span/><b>Appointment Time</b><br/>
-  //               ${appointment_details.date} ${appointment_details.time}<br/>
-  //               <b>Doctor</b><br/>${doctor.name}
-  //               <br/><br/>
-  //               <p>Hi ${invoiceData.name},</p>
-  //               <p><span>Here is a confirmation email that you we have scheduled your appointment with ${doctor.name} on ${appointment_details.date} at ${appointment_details.time}.<b/>
-  //               </p>`,
-  //     });
-  //     const mail = {
-  //       info: info,
-  //     };
-  //     return mail;
-  //   } catch (e) {
-  //     console.log("catching error in mail==>", e);
-  //     throw e;
-  //   }
-  // };
-  // informDoctor = async (invoiceData, appointment_details, patient) => {
-  //   try {
-  //     let info = await transporter.sendMail({
-  //       from: "", // sender address
-  //       to: "" + `${invoiceData.email}`, //receiver email
-  //       subject: "Telemed.App Appointment Confirmation", // Subject line
-  //       html: `
-  //               <br/>
-  //               <span/><b>Appointment Time</b><br/>
-  //               ${appointment_details.date} ${appointment_details.time}<br/><br/>
-  //               <b>Patient</b><br/>
-  //               ${patient.name}<br/>
-  //               <p>Hi ${invoiceData.name},</p>
-  //               <p><span>Here is a confirmation email that, we have scheduled your appointment with ${patient.name} on ${appointment_details.date} at ${appointment_details.time}.<b/>
-  //               </p>`,
-  //     });
-  //     const mail = {
-  //       info: info,
-  //     };
-  //     return mail;
-  //   } catch (e) {
-  //     console.log("catching error in mail==>", e);
-  //     throw e;
-  //   }
-  // };
+
   sendTestMail = async req => {
     try {
       var mailOptions1 = {
@@ -248,8 +133,8 @@ export class NodemailerService {
         `,
         attachments: [
           {
-            filename: 'Logo.png',
-            path: './logo.png',
+            filename: 'logo.png',
+            path: './assets/logo.png',
             cid: 'logo', //my mistake was putting "cid:logo@cid" here!
           },
         ],
@@ -266,20 +151,6 @@ export class NodemailerService {
       });
       console.log('check infor', info);
       return info;
-      // let info = await transporter.sendMail({
-      //   from: '', // sender address
-      //   to: '' + `${'samad13354@gmail.com'}`, //receiver email
-      //   subject: 'Telemed.App Appointment Confirmation', // Subject line
-      //   html: `
-      //                 <br/>
-      //                 <span/><b>Appointment Time</b><br/>
-      //                 </p>`,
-      // });
-      // const mail = {
-      //   info: info,
-      // };
-      // console.log('check mail now', mail);
-      // return mail;
     } catch (e) {
       console.log('catching error in mail==>', e);
       throw 'Email not Send';
